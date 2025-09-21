ae66bbd7bc0d511538500ba3b542ed6e1a0bb9a1
Ignacio <ignamosconi@gmail.com>
2025-09-21 15:28:37 -0300

    Pedido de cambio 2.2.2 - Migración a PSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import jwtConfig from './auth/jwt/jwt.config';
+import { UserEntity } from './users/entities/user.entity';
 
 @Module({
   imports: [
@@ -22,13 +23,13 @@ import jwtConfig from './auth/jwt/jwt.config';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
+        type: 'postgres',   //Se cambió de "mysql" a "posgres"
         host: configService.get<string>('DB_HOST'),
         port: configService.get<number>('DB_PORT'),
         username: configService.get<string>('DB_USERNAME'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_DATABASE'),
-        entities: [CalculoImc],
+        entities: [CalculoImc, UserEntity],
         synchronize: false,
         autoLoadEntities: true,
       }),
